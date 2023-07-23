50338ab960845a6ea2f2aa31a62d99ca723ec562
Sergey Georgiev <sgrgv_art@mail.ru>
2023-07-23 17:36:07 +0300

    Добавить nav с классом header__nav прописать стиль

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 //style
 import "./Header.css";
@@ -9,6 +10,25 @@ const Header = () => {
       <div className="header__logo">
         <img src="./images/logo.png" alt="logo" />
       </div>
+      <nav className="header__nav">
+        <ul>
+          <li>
+            <Link>Home</Link>
+          </li>
+          <li>
+            <Link>Products</Link>
+          </li>
+          <li>
+            <Link>About</Link>
+          </li>
+          <li>
+            <Link>Contacts</Link>
+          </li>
+          <li>
+            <Link>Account</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
