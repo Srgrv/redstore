dcad5602e34bcac88158f347d58f3cb88c783578
Sergey Georgiev <sgrgv_art@mail.ru>
2023-07-23 17:11:05 +0300

    добавить header в app и сделать лого

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
+//containers
+import Header from "./containers/Header/Header";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
