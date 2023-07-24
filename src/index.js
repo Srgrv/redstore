26701ae96230dd01c72849227f6d55a55db60f59
Sergey Georgiev <sgrgv_art@mail.ru>
2023-07-24 11:46:33 +0300

    импортировать стили

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+//style
+import "./index.css";
+
 //components
 import App from "./App";
 
