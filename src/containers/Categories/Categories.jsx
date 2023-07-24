c942298ced5ceb9ccb43d88c6d8e51b757244bb4
Sergey Georgiev <sgrgv_art@mail.ru>
2023-07-24 12:34:11 +0300

    добавить дополнительный контейнер с классом

--- src/containers/Categories/Categories.jsx
+++ src/containers/Categories/Categories.jsx
@@ -5,15 +5,17 @@ import "./Categories.css";
 const Categories = () => {
   return (
     <div className="categories">
-      <div className="categories__row">
-        <div className="categories__row-col">
-          <img src="./images/category-1.jpg" alt="category-1" />
-        </div>
-        <div className="categories__row-col">
-          <img src="./images/category-1.jpg" alt="category-1" />
-        </div>
-        <div className="categories__row-col">
-          <img src="./images/category-1.jpg" alt="category-1" />
+      <div className="small-container">
+        <div className="categories__row">
+          <div className="categories__row-col">
+            <img src="./images/category-1.jpg" alt="category-1" />
+          </div>
+          <div className="categories__row-col">
+            <img src="./images/category-2.jpg" alt="category-1" />
+          </div>
+          <div className="categories__row-col">
+            <img src="./images/category-3.jpg" alt="category-1" />
+          </div>
         </div>
       </div>
     </div>
