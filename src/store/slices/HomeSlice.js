807035aea9c3365428e99149c8413c71b8b3c027
Sergey Georgiev <sgrgv_art@mail.ru>
2023-08-02 19:22:11 +0300

    create reducer - toogleNav

--- src/store/slices/HomeSlice.js
+++ src/store/slices/HomeSlice.js
@@ -3,15 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 const HomeSlice = createSlice({
   name: "homeSlice",
   initialState: {
-    isNav: false;
+    isNav: false,
   },
   reducers: {
-    toggleNav(state, action) {
-        state.isNav = !state.isNav;
-    }
-  }
+    toggleNav(state) {
+      state.isNav = !state.isNav;
+    },
+  },
 });
 
-export const {toggleNav} = HomeSlice.actions; 
+export const { toggleNav } = HomeSlice.actions;
 
 export default HomeSlice.reducer;
