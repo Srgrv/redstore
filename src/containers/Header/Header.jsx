098b180c47cdef7c66679fbc8dcc590c12e6d9bc
Sergey Georgiev <sgrgv_art@mail.ru>
2023-08-03 03:24:35 +0300

    create Popup

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 //style
 import "./Header.css";
 
 const Header = () => {
+  const [menu_class, setMenuClass] = useState("header__nav-ul hidden");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+  const updateMenu = () => {
+    setMenuClass(
+      !isMenuClicked ? "header__nav-ul visible" : "header__nav-ul hidden"
+    );
+    setIsMenuClicked(!isMenuClicked);
+  };
+
   return (
     <div className="header">
       <div className="header__logo">
         <img src="./images/logo.png" alt="logo" />
       </div>
       <nav className="header__nav">
-        <ul>
+        <ul className={menu_class}>
           <li>
             <Link>Home</Link>
           </li>
@@ -31,7 +41,11 @@ const Header = () => {
       </nav>
 
       <img className="header__img" src="./images/cart.png" alt="cart" />
-      <img className="header__menuIcon" src="./images/menu.png" />
+      <img
+        onClick={updateMenu}
+        className="header__menuIcon"
+        src="./images/menu.png"
+      />
     </div>
   );
 };
