5d8248fe285de6cc5f4630ba66906bf4617ae37d
Sergey Georgiev <sgrgv_art@mail.ru>
2023-08-04 11:58:54 +0300

    create HomePage

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+//style
+import "./HomePage.css";
+
+//containers
+import Categories from "../../containers/Categories/Categories";
+import Products from "../../containers/Products/Products";
+import Offer from "../../containers/Offer/Offer";
+import Testimonial from "../../containers/Testimonial/Testimonial";
+import Brands from "../../containers/Brands/Brands";
+import Header from "../../containers/Header/Header";
+import Row from "../../containers/Row/Row";
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <div className="homePage__container">
+        <Header />
+        <Row />
+      </div>
+      <Categories />
+      <Products />
+      <Offer />
+      <Testimonial />
+      <Brands />
+    </div>
+  );
+};
+
+export default HomePage;
