0267e0bb7f2ff595b049e3c47ab758204f5ee32f
Sergey Georgiev <sgrgv_art@mail.ru>
2023-08-04 11:58:21 +0300

    import Provider and past it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 //style
 import "./index.css";
@@ -8,10 +9,15 @@ import "./index.css";
 //components
 import App from "./App";
 
+//store
+import store from "./store/store";
+
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
