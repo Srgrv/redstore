e06142c4beb97d897e2020e6f5206de7733a3e0b
Sergey Georgiev <sgrgv_art@mail.ru>
2023-08-04 12:00:01 +0300

    create ProductsPageSlice and past it to store

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 //slices
 import HomeSlice from "./slices/HomeSlice";
+import ProductsPageSlice from "./slices/ProductsPageSlice";
 
 const store = configureStore({
   reducer: {
     home: HomeSlice,
+    products: ProductsPageSlice,
   },
 });
 
