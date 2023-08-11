5b5cee9328c0890ae1ae96ba88394366146b4ac8
Sergey Georgiev <sgrgv_art@mail.ru>
2023-08-11 20:48:51 +0300

    add ProductsPage

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,20 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout/Layout";
 import HomePage from "./pages/HomePage/HomePage";
 import ProductsPage from "./pages/ProductsPage/ProductsPage";
+import TestPage from "./pages/TestPage/TestPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/products" element={<ProductsPage />} />
+        <Route path="products/:id" element={<ProductsPage />} />
+        <Route path="test" element={<TestPage />} />
       </Route>
     </Routes>
   );
 }
 
 export default App;
+
+//не работает роутинг
